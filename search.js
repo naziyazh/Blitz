8452719582baa6d9f02090a7bbc0bce03f917370
Naziya Zhakupova <sabrkyzy@gmail.com>
2020-06-19 23:43:01 +0900

    .

--- search.js
+++ search.js
@@ -1,18 +1,5 @@
 $( document ).ready(function() {
-    //database credentials
-    // var firebaseConfig = {
-    //     apiKey: "AIzaSyBs5aJhECS8s1bspUxdVk-hJH7FrlasDwc",
-    //     authDomain: "hci-project-817b4.firebaseapp.com",
-    //     databaseURL: "https://hci-project-817b4.firebaseio.com",
-    //     projectId: "hci-project-817b4",
-    //     storageBucket: "hci-project-817b4.appspot.com",
-    //     messagingSenderId: "1084392647243",
-    //     appId: "1:1084392647243:web:b8d31ad3108e563cd23bce",
-    //     measurementId: "G-D6VMGFT1W2"
-    //   };
-    //   // Initialize Firebase
-    //   firebase.initializeApp(firebaseConfig);
-
+   
     //get search exercise names from database
     
     $('.search-submit').click(function(){
